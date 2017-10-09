92fca728bfa2c9f1e4072bede2c116c424b303a6
martosaur <martosaur@gmail.com>
2017-10-09 19:47:13 +0200

    refactored commands.ex

--- lib/krksmogbot/commands.ex
+++ lib/krksmogbot/commands.ex
@@ -11,34 +11,24 @@ defmodule Krksmogbot.Commands do
                                                resize_keyboard: true}
     @city_center_coords %{latitude: 50.0611127, longitude: 19.9379205}
 
-    def match(%{message: %{chat: %{id: chat_id}, text: "/start"}}) do
-        Nadia.send_message(chat_id, @start_msg,
-                           [parse_mode: "Markdown",
-                            disable_web_page_preview: true,
-                            reply_markup: @keyboard])
-    end
-    def match(%{message: %{chat: %{id: chat_id}, text: "/legend"}}) do
-        Nadia.send_message(chat_id, @legend_msg,
-                           [parse_mode: "Markdown",
-                            disable_web_page_preview: true,
-                            reply_markup: @keyboard])
-    end
-    def match(%{message: %{chat: %{id: chat_id}, message_id: msg_id, location: %{latitude: lat, longitude: lng}}}) do
-        Nadia.send_message(chat_id, Measurements.get_measurements(lat, lng),
-                           [parse_mode: "Markdown",
-                            disable_web_page_preview: true,
-                            reply_to_message_id: msg_id])
-    end
-    def match(%{message: %{chat: %{id: chat_id}, text: "Check city center"}} = msg) do
-        msg.message.location
-        |> put_in(@city_center_coords)
-        |> match
-    end
-    def match(%{message: %{chat: %{id: chat_id}}}) do
-        Nadia.send_message(chat_id, @help_msg,
-                           [parse_mode: "Markdown",
-                            disable_web_page_preview: true,
-                            reply_markup: @keyboard])
+    def match(%{message: msg} = input) do
+        case msg do
+            %{text: "/start"} -> @start_msg |> send_message_with_keyboard(msg)
+            %{text: "/legend"} -> @legend_msg |> send_message_with_keyboard(msg)
+            %{location: %{latitude: lat, longitude: lng}, message_id: msg_id} -> Measurements.get_measurements(lat, lng) |> send_message_with_keyboard(msg, [reply_to_message_id: msg_id])
+            %{text: "Check city center"} -> 
+                input.message.location
+                |> put_in(@city_center_coords)
+                |> match
+            _ -> @help_msg |> send_message_with_keyboard(msg)
+        end
     end
     def match(backoff), do: Logger.log :error, "Unmatched message #{Poison.encode!(backoff)}"
+    
+    defp send_message_with_keyboard(text, %{chat: %{id: chat_id}}, options \\ []) do
+        Nadia.send_message(chat_id, text,
+                            [parse_mode: "Markdown",
+                             disable_web_page_preview: true,
+                             reply_markup: @keyboard] ++ options)
+    end
 end
