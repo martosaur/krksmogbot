c2e02694b38b77e70580db65df3a7966b574c403
martosaur <martosaur@gmail.com>
2017-11-12 14:13:17 +0100

    replaced single matcher process with Tasks + some polish

--- lib/botan/botan.ex
+++ lib/botan/botan.ex
@@ -1,26 +1,11 @@
 defmodule Botan do
-    use GenServer
-    require Logger
-
-    def start_link do
-        Logger.log :info, "Started botan"
-        GenServer.start_link __MODULE__, :ok, name: __MODULE__
+  require Logger
+  
+  def track(message) do
+    case message do
+      %{message: %{text: nil, from: %{username: uid}}} -> Botan.API.track(uid, "/location", message)
+      %{message: %{text: text, from: %{username: uid}}} -> Botan.API.track(uid, text, message)
+      r -> Logger.error("Could not report following message: #{inspect(message)}")
     end
-
-    def init(:ok) do
-        {:ok, 0}
-    end
-
-    def track(message) do
-        GenServer.cast __MODULE__, {:track, message}
-    end
-
-    def handle_cast({:track, message}, state) do
-        case message do
-            %{message: %{text: nil, from: %{username: uid}}} -> Botan.API.track(uid, "/location", message)
-            %{message: %{text: text, from: %{username: uid}}} -> Botan.API.track(uid, text, message)
-            r -> Logger.log :error, "Could not report following message: #{inspect(message)}"
-        end
-        {:noreply, state}
-    end
+  end
 end
---
+++ resources/about.md
@@ -0,0 +1,3 @@
+If you were wondering the bot's creator and maintainer is @martosaur
+
+The source code can be found here: https://github.com/martosaur/krksmogbot
